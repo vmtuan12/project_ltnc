eac3abb25aba7c3f8e6d858b67454e0d8fa4ad01
vmtuan12 <minhtuan3154@gmail.com>
2022-04-03 16:59:41 +0700

    va cham player enemy rocket

---
+++ file2/player.h
@@ -0,0 +1,26 @@
+#ifndef PLAYER_H
+#define PLAYER_H
+
+#include "baseObject.h"
+#include "rocket.h"
+
+class player : public baseObject
+{
+public:
+    player();
+    ~player();
+
+    void inputKey(SDL_Event e, SDL_Renderer *ren);
+    void loadPlayerAmmo(SDL_Renderer *ren);
+
+    void setAmmo(std::vector<rocket*> ammo_) {ammoNum = ammo_;}
+    std::vector<rocket*> getAmmo() const {return ammoNum;}
+    void eraseRocket(const int &pos);
+
+protected:
+    int x_val;
+    int y_val;
+    std::vector<rocket*> ammoNum;
+};
+
+#endif
